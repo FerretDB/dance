2aebfdf99732aa396fae049509628c1fbb0ae1cf
Ekaterina Blech <blech.kate@ya.ru>
2022-03-18 17:44:42 +0300

    test query operators divided

--- tests/ferret/query_operators.go
+++ tests/ferret/query_operators.go
@@ -21,241 +21,17 @@ var TestQueryOPerators = func(ctx context.Context, db *mongo.Database, t *testin
 		t.Parallel()
 
 		collection := db.Collection(collectionName(t))
-
-		// TODO keep in sync with test_db? https://github.com/FerretDB/dance/issues/43
-		data := map[string]any{
-			"double":                   42.13,
-			"double-zero":              0.0,
-			"double-max":               math.MaxFloat64,
-			"double-smallest":          math.SmallestNonzeroFloat64,
-			"double-positive-infinity": math.Inf(+1),
-			"double-negative-infinity": math.Inf(-1),
-			"double-nan":               math.NaN(),
-
-			"string":       "foo",
-			"string-empty": "",
-			// "\x00",
-
-			"document":       map[string]any{"document": int32(42)},
-			"document-empty": map[string]any{},
-
-			"array":       primitive.A{"array", int32(42)},
-			"array-empty": primitive.A{},
-			"array-embedded": primitive.A{
-				primitive.D{
-					primitive.E{Key: "age", Value: int32(1000)},
-					primitive.E{Key: "document", Value: "abc"},
-					primitive.E{Key: "score", Value: float32(42.13)},
-				},
-				primitive.D{
-					primitive.E{Key: "age", Value: int32(1000)},
-					primitive.E{Key: "document", Value: "def"},
-					primitive.E{Key: "score", Value: float32(42.13)},
-				},
-				primitive.D{
-					primitive.E{Key: "age", Value: int32(1002)},
-					primitive.E{Key: "document", Value: "jkl"},
-					primitive.E{Key: "score", Value: int32(24)},
-				},
-			},
-			"binary":       primitive.Binary{Subtype: 0x80, Data: []byte{42, 0, 13}},
-			"binary-empty": primitive.Binary{},
-
-			// no Undefined
-
-			"bool-false": false,
-			"bool-true":  true,
-
-			"datetime":          time.Date(2021, 11, 1, 10, 18, 42, 123000000, time.UTC),
-			"datetime-epoch":    time.Unix(0, 0),
-			"datetime-year-min": time.Date(0, 1, 1, 0, 0, 0, 0, time.UTC),
-			"datetime-year-max": time.Date(9999, 12, 31, 23, 59, 59, 999000000, time.UTC),
-
-			"null": nil,
-
-			"regex":       primitive.Regex{Pattern: "foo", Options: "i"},
-			"regex-empty": primitive.Regex{},
-
-			// no DBPointer
-			// no JavaScript code
-			// no Symbol
-			// no JavaScript code w/ scope
-
-			"int32":      int32(42),
-			"int32-zero": int32(0),
-			"int32-max":  int32(math.MaxInt32),
-			"int32-min":  int32(math.MinInt32),
-
-			"timestamp":   primitive.Timestamp{T: 42, I: 13},
-			"timestamp-i": primitive.Timestamp{I: 1},
-
-			"int64":      int64(42),
-			"int64-zero": int64(0),
-			"int64-max":  int64(math.MaxInt64),
-			"int64-min":  int64(math.MinInt64),
-
-			// no 128-bit decimal floating point (yet)
-
-			// no Min key
-			// no Max key
-		}
+		data := getTestDBData()
 
 		for id, v := range data {
 			_, err := collection.InsertOne(ctx, bson.D{{"_id", id}, {"value", v}})
 			require.NoError(t, err)
 		}
 
-		testCases := []struct {
-			name string // TODO move to map key
-			q    bson.D
-			o    *options.FindOptions // options
-			v    any
-			IDs  []string
-			err  error
-		}{
-			// doubles
-			// $eq
-			/*
-				{
-					bson.D{{"$eq", 42.13}},
-					[]string{"double"},
-				},
-				{
-					bson.D{{"$eq", math.Inf(-1)}},
-					[]string{"double-negative-infinity"},
-				},
-				{
-					bson.D{{"$eq", math.Inf(+1)}},
-					[]string{"double-positive-infinity"},
-				},
-				{
-					bson.D{{"$eq", math.MaxFloat64}},
-					[]string{"double-max"},
-				},
-				{
-					bson.D{{"$eq", math.SmallestNonzeroFloat64}},
-					[]string{"double-smallest"},
-				},
-				// $gt
-				{
-					bson.D{{"$gt", 42.123}},
-					[]string{"double-max", "double-positive-infinity"},
-				},
-				{
-					bson.D{{"$gt", math.Inf(-1)}},
-					[]string{"double-smallest", "double", "double-max", "double-positive-infinity"},
-				},
-				{
-					bson.D{{"$gt", math.Inf(+1)}},
-					nil,
-				},
-				{
-					bson.D{{"$gt", math.MaxFloat64}},
-					[]string{"double-positive-infinity"},
-				},
-				{
-					bson.D{{"$gt", math.SmallestNonzeroFloat64}},
-					[]string{"double", "double-max", "double-positive-infinity"},
-				},
-			*/
-
-			// strings
-			/*
-				{
-					bson.D{{"$eq", "foo"}},
-					[]string{"string"},
-				},
-				{
-					bson.D{{"$gt", "foo"}},
-					[]string{},
-				},
-			*/
-
-			// documents
-			// TODO
-
-			// arrays
-			// $size
-			{
-				name: "SizeInt32",
-				q:    bson.D{{"value", bson.D{{"$size", int32(2)}}}},
-				IDs:  []string{"array"},
-			},
-			{
-				name: "SizeInt64",
-				q:    bson.D{{"value", bson.D{{"$size", int64(2)}}}},
-				IDs:  []string{"array"},
-			},
-			{
-				name: "SizeDouble",
-				q:    bson.D{{"value", bson.D{{"$size", 2.0}}}},
-				IDs:  []string{"array"},
-			},
-			{
-				name: "SizeNotFound",
-				q:    bson.D{{"value", bson.D{{"$size", int32(4)}}}},
-				IDs:  []string{},
-			},
-			{
-				name: "SizeZero",
-				q:    bson.D{{"value", bson.D{{"$size", 0.0}}}},
-				IDs:  []string{"array-empty"},
-			},
-			{
-				name: "SizeInvalidType",
-				q:    bson.D{{"value", bson.D{{"$size", bson.D{{"$gt", int32(1)}}}}}},
-				err: mongo.CommandError{
-					Code:    2,
-					Name:    "BadValue",
-					Message: `$size needs a number`,
-				},
-			},
-			{
-				name: "SizeNonWhole",
-				q:    bson.D{{"value", bson.D{{"$size", 2.1}}}},
-				err: mongo.CommandError{
-					Code:    2,
-					Name:    "BadValue",
-					Message: `$size must be a whole number`,
-				},
-			},
-			{
-				name: "SizeNaN",
-				q:    bson.D{{"value", bson.D{{"$size", math.NaN()}}}},
-				err: mongo.CommandError{
-					Code:    2,
-					Name:    "BadValue",
-					Message: `$size must be a whole number`,
-				},
-			},
-			{
-				name: "SizeInfinity",
-				q:    bson.D{{"value", bson.D{{"$size", math.Inf(1)}}}},
-				err: mongo.CommandError{
-					Code:    2,
-					Name:    "BadValue",
-					Message: `$size must be a whole number`,
-				},
-			},
-			{
-				name: "SizeNegative",
-				q:    bson.D{{"value", bson.D{{"$size", int32(-1)}}}},
-				err: mongo.CommandError{
-					Code:    2,
-					Name:    "BadValue",
-					Message: `$size may not be negative`,
-				},
-			},
-			{
-				q: bson.D{{"$size", int32(2)}},
-				err: mongo.CommandError{
-					Code: 2,
-					Name: "BadValue",
-					Message: `unknown top level operator: $size. If you have a field name that starts with a '$' symbol, ` +
-						`consider using $getField or $setField.`,
-				},
-			},
-		}
+		var testCases []testCase
+		// testCases = append(testCases, doublesEq...)
+		// testCases = append(testCases, stringCases...)
+		testCases = append(testCases, arraySize...)
 
 		for _, tc := range testCases {
 			tc := tc
@@ -305,4 +81,231 @@ var TestQueryOPerators = func(ctx context.Context, db *mongo.Database, t *testin
 			})
 		}
 	}
+}
+
+type testCase struct {
+	name string // TODO move to map key
+	q    bson.D
+	o    *options.FindOptions
+	v    any
+	IDs  []string
+	err  error
+}
+
+var doublesEq = []testCase{
+	{
+		q:   bson.D{{"$eq", 42.13}},
+		IDs: []string{"double"},
+	},
+	{
+		q:   bson.D{{"$eq", math.Inf(-1)}},
+		IDs: []string{"double-negative-infinity"},
+	},
+	{
+		q:   bson.D{{"$eq", math.Inf(+1)}},
+		IDs: []string{"double-positive-infinity"},
+	},
+	{
+		q:   bson.D{{"$eq", math.MaxFloat64}},
+		IDs: []string{"double-max"},
+	},
+	{
+		q:   bson.D{{"$eq", math.SmallestNonzeroFloat64}},
+		IDs: []string{"double-smallest"},
+	},
+	// $gt
+	{
+		q:   bson.D{{"$gt", 42.123}},
+		IDs: []string{"double-max", "double-positive-infinity"},
+	},
+	{
+		q:   bson.D{{"$gt", math.Inf(-1)}},
+		IDs: []string{"double-smallest", "double", "double-max", "double-positive-infinity"},
+	},
+	{
+		q:   bson.D{{"$gt", math.Inf(+1)}},
+		IDs: nil,
+	},
+	{
+		q:   bson.D{{"$gt", math.MaxFloat64}},
+		IDs: []string{"double-positive-infinity"},
+	},
+	{
+		q:   bson.D{{"$gt", math.SmallestNonzeroFloat64}},
+		IDs: []string{"double", "double-max", "double-positive-infinity"},
+	},
+}
+
+var stringCases = []testCase{
+	{
+		q:   bson.D{{"$eq", "foo"}},
+		IDs: []string{"string"},
+	},
+	{
+		q:   bson.D{{"$gt", "foo"}},
+		IDs: []string{},
+	},
+}
+
+var arraySize = []testCase{
+	{
+		name: "SizeInt32",
+		q:    bson.D{{"value", bson.D{{"$size", int32(2)}}}},
+		IDs:  []string{"array"},
+	},
+	{
+		name: "SizeInt64",
+		q:    bson.D{{"value", bson.D{{"$size", int64(2)}}}},
+		IDs:  []string{"array"},
+	},
+	{
+		name: "SizeDouble",
+		q:    bson.D{{"value", bson.D{{"$size", 2.0}}}},
+		IDs:  []string{"array"},
+	},
+	{
+		name: "SizeNotFound",
+		q:    bson.D{{"value", bson.D{{"$size", int32(4)}}}},
+		IDs:  []string{},
+	},
+	{
+		name: "SizeZero",
+		q:    bson.D{{"value", bson.D{{"$size", 0.0}}}},
+		IDs:  []string{"array-empty"},
+	},
+	{
+		name: "SizeInvalidType",
+		q:    bson.D{{"value", bson.D{{"$size", bson.D{{"$gt", int32(1)}}}}}},
+		err: mongo.CommandError{
+			Code:    2,
+			Name:    "BadValue",
+			Message: `$size needs a number`,
+		},
+	},
+	{
+		name: "SizeNonWhole",
+		q:    bson.D{{"value", bson.D{{"$size", 2.1}}}},
+		err: mongo.CommandError{
+			Code:    2,
+			Name:    "BadValue",
+			Message: `$size must be a whole number`,
+		},
+	},
+	{
+		name: "SizeNaN",
+		q:    bson.D{{"value", bson.D{{"$size", math.NaN()}}}},
+		err: mongo.CommandError{
+			Code:    2,
+			Name:    "BadValue",
+			Message: `$size must be a whole number`,
+		},
+	},
+	{
+		name: "SizeInfinity",
+		q:    bson.D{{"value", bson.D{{"$size", math.Inf(1)}}}},
+		err: mongo.CommandError{
+			Code:    2,
+			Name:    "BadValue",
+			Message: `$size must be a whole number`,
+		},
+	},
+	{
+		name: "SizeNegative",
+		q:    bson.D{{"value", bson.D{{"$size", int32(-1)}}}},
+		err: mongo.CommandError{
+			Code:    2,
+			Name:    "BadValue",
+			Message: `$size may not be negative`,
+		},
+	},
+	{
+		q: bson.D{{"$size", int32(2)}},
+		err: mongo.CommandError{
+			Code: 2,
+			Name: "BadValue",
+			Message: `unknown top level operator: $size. If you have a field name that starts with a '$' symbol, ` +
+				`consider using $getField or $setField.`,
+		},
+	},
+}
+
+func getTestDBData() (data map[string]any) {
+	// TODO keep in sync with test_db? https://github.com/FerretDB/dance/issues/43
+	data = map[string]any{
+		"double":                   42.13,
+		"double-zero":              0.0,
+		"double-max":               math.MaxFloat64,
+		"double-smallest":          math.SmallestNonzeroFloat64,
+		"double-positive-infinity": math.Inf(+1),
+		"double-negative-infinity": math.Inf(-1),
+		"double-nan":               math.NaN(),
+
+		"string":       "foo",
+		"string-empty": "",
+		// "\x00",
+
+		"document":       map[string]any{"document": int32(42)},
+		"document-empty": map[string]any{},
+
+		"array":       primitive.A{"array", int32(42)},
+		"array-empty": primitive.A{},
+		"array-embedded": primitive.A{
+			primitive.D{
+				primitive.E{Key: "age", Value: int32(1000)},
+				primitive.E{Key: "document", Value: "abc"},
+				primitive.E{Key: "score", Value: float32(42.13)},
+			},
+			primitive.D{
+				primitive.E{Key: "age", Value: int32(1000)},
+				primitive.E{Key: "document", Value: "def"},
+				primitive.E{Key: "score", Value: float32(42.13)},
+			},
+			primitive.D{
+				primitive.E{Key: "age", Value: int32(1002)},
+				primitive.E{Key: "document", Value: "jkl"},
+				primitive.E{Key: "score", Value: int32(24)},
+			},
+		},
+		"binary":       primitive.Binary{Subtype: 0x80, Data: []byte{42, 0, 13}},
+		"binary-empty": primitive.Binary{},
+
+		// no Undefined
+
+		"bool-false": false,
+		"bool-true":  true,
+
+		"datetime":          time.Date(2021, 11, 1, 10, 18, 42, 123000000, time.UTC),
+		"datetime-epoch":    time.Unix(0, 0),
+		"datetime-year-min": time.Date(0, 1, 1, 0, 0, 0, 0, time.UTC),
+		"datetime-year-max": time.Date(9999, 12, 31, 23, 59, 59, 999000000, time.UTC),
+
+		"null": nil,
+
+		"regex":       primitive.Regex{Pattern: "foo", Options: "i"},
+		"regex-empty": primitive.Regex{},
+
+		// no DBPointer
+		// no JavaScript code
+		// no Symbol
+		// no JavaScript code w/ scope
+
+		"int32":      int32(42),
+		"int32-zero": int32(0),
+		"int32-max":  int32(math.MaxInt32),
+		"int32-min":  int32(math.MinInt32),
+
+		"timestamp":   primitive.Timestamp{T: 42, I: 13},
+		"timestamp-i": primitive.Timestamp{I: 1},
+
+		"int64":      int64(42),
+		"int64-zero": int64(0),
+		"int64-max":  int64(math.MaxInt64),
+		"int64-min":  int64(math.MinInt64),
+
+		// no 128-bit decimal floating point (yet)
+
+		// no Min key
+		// no Max key
+	}
+	return
 }
