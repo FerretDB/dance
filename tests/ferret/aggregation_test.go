296e87c489323a1c42773df2142a641f946359f9
w84thesun <dmitry.eremenko@ferretdb.io>
2022-03-22 11:47:37 +0300

    Add test for sort aggregation

---
+++ tests/ferret/aggregation_test.go
@@ -0,0 +1,105 @@
+package ferret
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func TestAggregate(t *testing.T) {
+	t.Parallel()
+
+	ctx, db := setup(t)
+
+	t.Run("SortAggregate", func(t *testing.T) {
+		t.Parallel()
+
+		collection := db.Collection(collectionName(t))
+
+		data := []bson.D{
+			{{"_id", "1"}, {"name", "Central Park Cafe"}, {"borough", "Manhattan"}},
+			{{"_id", "2"}, {"name", "Rock A Feller Bar and Grill"}, {"borough", "Queens"}},
+			{{"_id", "3"}, {"name", "Empire State Pub"}, {"borough", "Brooklyn"}},
+			{{"_id", "4"}, {"name", "Stan's Pizzaria"}, {"borough", "Manhattan"}},
+			{{"_id", "5"}, {"name", "Jane's Deli"}, {"borough", "Brooklyn"}},
+		}
+
+		for _, v := range data {
+			_, err := collection.InsertOne(ctx, v)
+			require.NoError(t, err)
+		}
+
+		testCases := []struct {
+			name string
+			sort bson.D
+			IDs  []string
+			err  error
+		}{
+			{
+				name: "SortBoroughs",
+				sort: bson.D{{"borough", int32(1)}},
+				IDs:  []string{"3", "5", "1", "4", "2"},
+			},
+			{
+				name: "SortBoroughsAndNames",
+				sort: bson.D{{"borough", int32(1)}, {"name", int32(1)}},
+				IDs:  []string{"3", "5", "1", "4", "2"},
+			},
+			{
+				name: "SortBoroughsAndNamesPassInt64Value",
+				sort: bson.D{{"borough", int64(1)}, {"name", int64(1)}},
+				IDs:  []string{"3", "5", "1", "4", "2"},
+			},
+		}
+
+		for _, tc := range testCases {
+			tc := tc
+
+			if tc.name == "" {
+				tc.name = fmt.Sprint(tc.sort)
+			}
+
+			t.Run(tc.name, func(t *testing.T) {
+				t.Parallel()
+
+				if (tc.IDs == nil) == (tc.err == nil) {
+					t.Fatal("exactly one of IDs or err must be set")
+				}
+
+				cursor, err := collection.Find(ctx, bson.D{}, options.Find().SetSort(tc.sort))
+
+				if tc.err != nil {
+					require.Error(t, err)
+					require.Equal(t, tc.err, err)
+					return
+				}
+
+				require.NoError(t, err)
+				require.NotNil(t, cursor)
+
+				var actual []bson.D
+				require.NoError(t, cursor.All(ctx, &actual))
+				var actualIDs []string
+				for _, d := range actual {
+					for k, v := range d.Map() {
+						if k == "_id" {
+							v, ok := v.(string)
+							if !ok {
+								t.Fatal("bad _id value", v)
+							}
+
+							actualIDs = append(actualIDs, v)
+							break
+						}
+					}
+				}
+
+				assert.Equal(t, tc.IDs, actualIDs)
+			})
+		}
+	})
+}
