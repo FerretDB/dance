f69cf1e895ba5cf757a657d98631198fa66a328c
Ekaterina Blech <ekaterina.blech@ferretdb.io>
2022-07-06 14:49:53 +0300

    wip

---
+++ tests/diff/collection_name_test.go
@@ -0,0 +1,111 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package diff
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TestCollectionName documents difference in reposnes:
+//   * dots
+//   * dashes
+//   * max length in FerretDB: 255, in MongoDB: 63.
+//   * FerretDB reserved prefix is _ferretdb_, MongoDB - system..
+func TestCollectionName(t *testing.T) {
+	t.Parallel()
+
+	ctx, db := setup(t)
+	db = db.Client().Database("admin")
+
+	t.Run("Dot", func(t *testing.T) {
+		collection := "collection_name_with_a_dot."
+
+		t.Run("FerretDB", func(t *testing.T) {
+			err := db.CreateCollection(ctx, collection)
+			expected := fmt.Errorf(`Invalid collection name: 'testcollectionname-err.collection_name_with_a-dot.'`)
+			assert.Equal(t, expected, err)
+		})
+
+		t.Run("MongoDB", func(t *testing.T) {
+			err := db.CreateCollection(ctx, collection)
+			require.NoError(t, err)
+		})
+	})
+
+	t.Run("Dash", func(t *testing.T) {
+		collection := "collection_name_with_a-dash"
+
+		t.Run("FerretDB", func(t *testing.T) {
+			err := db.CreateCollection(ctx, collection)
+			expected := fmt.Errorf(`Invalid collection name: 'testcollectionname-err.collection_name_with_a-dash'`)
+			assert.Equal(t, expected, err)
+		})
+
+		t.Run("MongoDB", func(t *testing.T) {
+			err := db.CreateCollection(ctx, collection)
+			require.NoError(t, err)
+		})
+	})
+
+	t.Run("Length65", func(t *testing.T) {
+		collection := strings.Repeat("a", 65)
+
+		t.Run("FerretDB", func(t *testing.T) {
+			err := db.CreateCollection(ctx, collection)
+			require.NoError(t, err)
+		})
+
+		t.Run("MongoDB", func(t *testing.T) {
+			err := db.CreateCollection(ctx, collection)
+			expected := fmt.Errorf(`Invalid collection name: 'testcollectionname-err.%s'`, collection)
+			assert.Equal(t, expected, err)
+		})
+	})
+
+	t.Run("ReservedPrefixes", func(t *testing.T) {
+		collection := "_ferretdb_xxx"
+		t.Run("_ferretdb_", func(t *testing.T) {
+			t.Run("FerretDB", func(t *testing.T) {
+				err := db.CreateCollection(ctx, collection)
+				expected := fmt.Errorf(`Invalid collection name: 'testcollectionname-err.%s'`, collection)
+				assert.Equal(t, expected, err)
+			})
+
+			t.Run("MongoDB", func(t *testing.T) {
+				err := db.CreateCollection(ctx, collection)
+				require.NoError(t, err)
+			})
+		})
+
+		collection = "system."
+		t.Run("system", func(t *testing.T) {
+			t.Run("FerretDB", func(t *testing.T) {
+				err := db.CreateCollection(ctx, collection)
+				require.NoError(t, err)
+			})
+
+			t.Run("MongoDB", func(t *testing.T) {
+				err := db.CreateCollection(ctx, collection)
+				expected := fmt.Errorf(`Invalid collection name: 'testcollectionname-err.%s'`, collection)
+				assert.Equal(t, expected, err)
+			})
+		})
+	})
+}
