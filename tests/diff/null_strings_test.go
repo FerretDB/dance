d15d037375ec3f006311fab77329b522d6cb9e96
w84thesun <dmitry.eremenko@ferretdb.io>
2022-07-08 16:51:22 +0300

    WIP

--- tests/diff/null_strings_test.go
+++ tests/diff/null_strings_test.go
@@ -18,7 +18,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
@@ -36,6 +35,6 @@ func TestNullStrings(t *testing.T) {
 	})
 
 	t.Run("MongoDB", func(t *testing.T) {
-		require.NoError(t, err)
+		assert.Equal(t, err, nil)
 	})
 }
