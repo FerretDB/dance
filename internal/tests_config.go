c16e3d4f0bfad02fb13081216d60d6a941024a2b
noisersup <patryk@kwiatek.xyz>
2022-07-11 20:38:46 +0200

    wip

--- internal/tests_config.go
+++ internal/tests_config.go
@@ -72,62 +72,6 @@ type Tests struct {
 	OutRegex  []string // regexps that match the tests output (i.e. "^server version \"5.0.9\" is (lower|higher).*")
 }
 
-// FileTestsConfig is a yaml representation of the TestsConfig struct.
-// It differs from it by using "any" type to be able to parse maps (i.e. "- output_regex: ...").
-//
-// To gain a data the struct should be first converted to TestsConfig with FileTestsConfig.Convert() function.
-type FileTestsConfig struct {
-	Default status `yaml:"default"`
-	Stats   *Stats `yaml:"stats"`
-	Pass    []any  `yaml:"pass"`
-	Skip    []any  `yaml:"skip"`
-	Fail    []any  `yaml:"fail"`
-}
-
-// Convert converts a FileTestsConfig to TestsConfig struct.
-func (ftc *FileTestsConfig) Convert() (*TestsConfig, error) {
-	if ftc == nil {
-		return nil, nil // not sure if that works
-	}
-	tc := TestsConfig{ftc.Default, ftc.Stats, Tests{}, Tests{}, Tests{}}
-	//nolint:govet // we don't care about alignment there
-	for _, tcat := range []struct {
-		inTests  []any
-		outTests *Tests
-	}{
-		{ftc.Pass, &tc.Pass},
-		{ftc.Skip, &tc.Skip},
-		{ftc.Fail, &tc.Fail},
-	} {
-		for _, t := range tcat.inTests {
-			switch test := t.(type) {
-			case map[string]any:
-				mValue, ok := test["output_regex"]
-				if !ok {
-					return nil, fmt.Errorf("invalid field name (\"output_regex\" expected)")
-				}
-				regexp, ok := mValue.(string)
-				if !ok {
-					// Check specifically for an array
-					if _, ok := mValue.([]string); ok {
-						return nil, fmt.Errorf("invalid syntax: regexp value shouldn't be an array")
-					}
-					return nil, fmt.Errorf("invalid syntax: expected string, got: %T", mValue)
-				}
-
-				tcat.outTests.OutRegex = append(tcat.outTests.OutRegex, regexp)
-				continue
-			case string:
-				tcat.outTests.TestNames = append(tcat.outTests.TestNames, test)
-				continue
-			default:
-				return nil, fmt.Errorf("invalid type of %[1]q: %[1]T", t)
-			}
-		}
-	}
-	return &tc, nil
-}
-
 func (tc *TestsConfig) toMap() (map[string]status, error) {
 	res := make(map[string]status, len(tc.Pass.TestNames)+len(tc.Skip.TestNames)+len(tc.Fail.TestNames))
 
