2e43aefd6b983f793c18210bb8ced37a066f1398
noisersup <patryk@kwiatek.xyz>
2022-07-11 20:13:01 +0200

    wip

--- internal/tests_config.go
+++ internal/tests_config.go
@@ -55,6 +55,7 @@ type Stats struct {
 }
 
 // TestsConfig represents a part of the dance configuration for tests.
+// TODO: purpose and cases where it's used.
 //
 // May contain prefixes; the longest prefix wins.
 type TestsConfig struct {
@@ -65,11 +66,16 @@ type TestsConfig struct {
 	Fail    Tests
 }
 
+// Tests are the tests from yaml category pass / fail / skip.
 type Tests struct {
-	TestNames []string
-	OutRegex  []string
+	TestNames []string // tests names (i.e. "go.mongodb.org/mongo-driver/mongo/...")
+	OutRegex  []string // regexps that match the tests output (i.e. "^server version \"5.0.9\" is (lower|higher).*")
 }
 
+// FileTestsConfig is a yaml representation of the TestsConfig struct.
+// It differs from it by using "any" type to be able to parse maps (i.e. "- output_regex: ...").
+//
+// To gain a data the struct should be first converted to TestsConfig with FileTestsConfig.Convert() function.
 type FileTestsConfig struct {
 	Default status `yaml:"default"`
 	Stats   *Stats `yaml:"stats"`
@@ -154,7 +160,7 @@ type CompareResult struct {
 	Stats          Stats
 }
 
-// Compiles result output with expected outputs and return expected status.
+// getExpectedStatusRegex compiles result output with expected outputs and return expected status.
 // If no output matches expected - returns nil.
 func (tc *TestsConfig) getExpectedStatusRegex(result *TestResult) *status {
 	for _, expectedRes := range []struct {
