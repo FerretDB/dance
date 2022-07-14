bb78a2e6282400793162c75358b2081329dd0f86
noisersup <patryk@kwiatek.xyz>
2022-07-14 17:47:13 +0200

    wip

--- testdata/duplicates/test_test.go
+++ testdata/duplicates/test_test.go
@@ -6,6 +6,9 @@ import (
 
 func TestDuplicates(t *testing.T) {
 	t.Parallel()
+	// TODO: We should add here some test cases which would just Pass/Fail or return output required for tests
+
+	//TestFoo is the last part of the testname in the yaml file.
 	t.Run("TestFoo", func(t *testing.T) {
 	})
 }
