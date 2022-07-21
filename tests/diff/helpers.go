af18f9abef3bd212e0504d922fd31f0a11907ad2
Katerina Bletsch <ekaterina.blech@ferretdb.io>
2022-07-21 14:57:53 +0300

    Validate database names (#176)
    Closes FerretDB/FerretDB#807.
    
    Refs FerretDB/FerretDB#913.

--- tests/diff/helpers.go
+++ tests/diff/helpers.go
@@ -37,3 +37,33 @@ func AssertEqualError(t testing.TB, expected mongo.CommandError, actual error) b
 
 	return assert.Equal(t, expected, a)
 }
+
+// AssertEqualAltError asserts that the expected error is the same as the actual (ignoring the Raw part);
+// the alternative error message may be provided if FerretDB is unable to produce exactly the same text as MongoDB.
+//
+// In general, error messages should be the same. Exceptions include:
+//
+//  * MongoDB typos (e.g. "sortto" instead of "sort to");
+//  * MongoDB values formatting (e.g. we don't want to write additional code to format
+//    `{ $slice: { a: { b: 3 }, b: "string" } }` exactly the same way).
+//
+// In any case, the alternative error message returned by FerretDB should not mislead users.
+func AssertEqualAltError(t testing.TB, expected mongo.CommandError, altMessage string, actual error) bool {
+	t.Helper()
+
+	a, ok := actual.(mongo.CommandError)
+	if !ok {
+		return assert.Equal(t, expected, actual)
+	}
+
+	// set expected fields that might be helpful in the test output
+	require.Nil(t, expected.Raw)
+	expected.Raw = a.Raw
+
+	if assert.ObjectsAreEqual(expected, a) {
+		return true
+	}
+
+	expected.Message = altMessage
+	return assert.Equal(t, expected, a)
+}
