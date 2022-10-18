e8dbe4cdf8b28f6f542f8d2c9e6872d80b306db4
Elena Grahovac <elena@grahovac.me>
2022-10-18 13:14:59 +0200

    Add simplest validation to check data documents (#211)
    Closes FerretDB/FerretDB#693.

---
+++ tests/diff/document_validation_test.go
@@ -0,0 +1,83 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package diff
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func TestDocumentValidation(t *testing.T) {
+	t.Parallel()
+
+	ctx, db := setup(t)
+
+	t.Run("Insert", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := db.Collection("validation").InsertOne(ctx, bson.D{{"foo$", "bar"}})
+
+		t.Run("FerretDB", func(t *testing.T) {
+			t.Parallel()
+
+			expected := mongo.CommandError{
+				Code:    2,
+				Name:    "BadValue",
+				Message: `Invalid document, reason: invalid key: "foo$" (key must not contain $).`,
+			}
+			AssertEqualError(t, expected, err)
+		})
+
+		t.Run("MongoDB", func(t *testing.T) {
+			t.Parallel()
+
+			require.NoError(t, err)
+		})
+	})
+
+	t.Run("Update", func(t *testing.T) {
+		t.Parallel()
+
+		// initiate a collection with a valid document, so we have something to update
+		collection := db.Collection("validation")
+		_, err := collection.InsertOne(ctx, bson.D{
+			{"_id", "valid"},
+			{"v", int32(42)},
+		})
+		require.NoError(t, err)
+
+		_, err = collection.UpdateOne(ctx, bson.D{}, bson.D{{"$set", bson.D{{"foo$", "bar"}}}})
+
+		t.Run("FerretDB", func(t *testing.T) {
+			t.Parallel()
+
+			expected := mongo.CommandError{
+				Code:    2,
+				Name:    "BadValue",
+				Message: `Invalid document, reason: invalid key: "foo$" (key must not contain $).`,
+			}
+			AssertEqualError(t, expected, err)
+		})
+
+		t.Run("MongoDB", func(t *testing.T) {
+			t.Parallel()
+
+			require.NoError(t, err)
+		})
+	})
+}
