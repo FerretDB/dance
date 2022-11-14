6eb9ccbe54263cd9cec5dce85af5db91b4679ebd
noisersup <patryk@kwiatek.xyz>
2022-11-14 20:33:41 +0100

    wip

--- tests/diff/freemonitoring_test.go
+++ tests/diff/freemonitoring_test.go
@@ -1,13 +1,26 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package diff
 
 import (
 	"testing"
 
-	"golang.org/x/exp/slices"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/exp/slices"
 )
 
 func TestFreeMonitoring(t *testing.T) {
@@ -35,8 +48,8 @@ func TestFreeMonitoring(t *testing.T) {
 			case "state":
 				assert.Equal(t, "enabled", v)
 			case "message":
-				msg := "To see your monitoring data, navigate to the unique URL below. Anyone you share the URL with " +
-					"will also be able to view this page. You can disable monitoring at any time by running db.disableFreeMonitoring()."
+				msg := "To see your monitoring data, navigate to the unique URL below. Anyone you share the URL with will also " +
+					"be able to view this page. You can disable monitoring at any time by running db.disableFreeMonitoring()."
 				assert.Equal(t, msg, v)
 			case "ok":
 				assert.Equal(t, float64(1), v)
