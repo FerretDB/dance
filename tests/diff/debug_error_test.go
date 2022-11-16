4a2b321d4267abcdd986de3b4e95edd484fdc375
noisersup <patryk@kwiatek.xyz>
2022-11-16 14:59:43 +0100

    wip

--- tests/diff/debug_error_test.go
+++ tests/diff/debug_error_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func TestDebugError(t *testing.T) {
@@ -14,41 +15,62 @@ func TestDebugError(t *testing.T) {
 
 	for name, tc := range map[string]struct {
 		input       string
-		expectedErr error
+		expectedErr *mongo.CommandError
 		expectedRes bson.D
 	}{
 		"ok": {
-			input: "ok",
-		},
-		"panic": {
-			input: "panic",
+			input:       "ok",
+			expectedRes: bson.D{{"ok", float64(1)}},
 		},
 		"codeZero": {
 			input: "0",
+			expectedErr: &mongo.CommandError{
+				Code:    1,
+				Name:    "InternalError",
+				Message: "Unset",
+			},
 		},
 		"codeOne": {
 			input: "1",
+			expectedErr: &mongo.CommandError{
+				Code:    1,
+				Name:    "InternalError",
+				Message: "InternalError",
+			},
 		},
 		"codeNotLabeled": {
 			input: "33333",
+			expectedErr: &mongo.CommandError{
+				Code:    1,
+				Name:    "InternalError",
+				Message: "ErrorCode(33333)",
+			},
+		},
+		"string": {
+			input: "foo",
+			expectedErr: &mongo.CommandError{
+				Code:    1,
+				Name:    "InternalError",
+				Message: "foo",
+			},
 		},
 	} {
 		name, tc := name, tc
 
 		t.Run(name, func(t *testing.T) {
-			t.Parallel()
+			//t.Parallel()
 			var actual bson.D
 			err := db.RunCommand(ctx, bson.D{{"debugError", tc.input}}).Decode(&actual)
 
 			t.Run("FerretDB", func(t *testing.T) {
+
 				if tc.expectedErr != nil {
-					assert.Equal(t, tc.expectedErr, err)
+					AssertEqualError(t, *tc.expectedErr, err)
 					return
 				}
 				assert.NoError(t, err)
 
 				assert.Equal(t, tc.expectedRes, actual)
-
 			})
 		})
 	}
