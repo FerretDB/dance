4534442440d2b025e1cb76eb74d4be1719392738
noisersup <patryk@kwiatek.xyz>
2022-11-25 13:27:40 +0100

    wip

--- tests/mongo-tools/setup.go
+++ tests/mongo-tools/setup.go
@@ -1,34 +1,9 @@
 package diff
 
 import (
-	"context"
 	"testing"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
-	"github.com/stretchr/testify/require"
 )
 
 func setupMongoContainer(t *testing.T) {
 	t.Helper()
-	// TODO ensure FerretDB's `task run` and ferretdb_mongodb compatibility
-	cli, err := client.NewEnvClient()
-	require.NoError(err)
-
-	ctx := context.Background()
-
-	conf := types.ExecConfig{
-		AttachStdin:  true,
-		AttachStdout: true,
-		AttachStderr: true,
-
-		Cmd: []string{"mongodump", "-c", "test"}
-	}
-
-	resp, err := cli.ContainerExecAttach(ctx, "dance_mongodb", types.ExecStartCheck{
-		// TODO Tty
-	})
-	// TODO wait for container
-	require.NoError(t, err)
-
 }
