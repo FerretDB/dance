dda8bef64ffa24f423b2e47a5e86808c72a5d614
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2022-12-20 18:59:45 +0400

    Sync helpers

--- tests/diff/helpers.go
+++ tests/diff/helpers.go
@@ -30,9 +30,12 @@ import (
 func databaseName(tb testing.TB) string {
 	tb.Helper()
 
+	// database names are always lowercase
 	name := strings.ToLower(tb.Name())
-	name = strings.ReplaceAll(name, "/", "_")
+
+	name = strings.ReplaceAll(name, "/", "-")
 	name = strings.ReplaceAll(name, " ", "_")
+	name = strings.ReplaceAll(name, "$", "_")
 
 	require.Less(tb, len(name), 64)
 	return name
