c207d263e52b28c69dcaa094987720cf9276fd83
noisersup <patryk@kwiatek.xyz>
2022-12-28 17:32:52 +0100

    wip

--- tests/mongo-tools/export_import_test.go
+++ tests/mongo-tools/export_import_test.go
@@ -15,11 +15,54 @@
 package mongotools
 
 import (
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestExportImport(t *testing.T) {
 	t.Parallel()
+
+	localTestsRoot := filepath.Join("..", "..", "dumps", "tests")
+
+	type testCase struct {
+		name0          string
+		dbName         string
+		documentsCount int // document count
+	}
+
+	for _, tc := range []testCase{{
+		name0:          "shipwrecks.json",
+		dbName:         "sample_geospatial",
+		documentsCount: 11095,
+	}, {
+		name0:          "accounts.json",
+		dbName:         "sample_analytics",
+		documentsCount: 1746,
+	}} {
+		tc := tc
+		t.Run(tc.name0, func(t *testing.T) {
+
+			ctx, client := setup(t)
+
+			name1 := tc.dbName + tc.name0 + "_dump1"
+			name2 := tc.dbName + tc.name0 + "_dump2"
+
+			// pre-create directory to avoid permission issues
+			recreateDir(t, filepath.Join(localTestsRoot, name1))
+
+			db1 := client.Database(name1)
+			t.Cleanup(func() { require.NoError(t, db1.Drop(ctx)) })
+
+			db2 := client.Database(name2)
+			t.Cleanup(func() { require.NoError(t, db2.Drop(ctx)) })
+
+			// source file -> db1
+			mongoimport(t, "", tc.dbName, tc.name0)
+		})
+	}
+
 }
 
 // mongoimport imports collection from <file> file as <db>/<coll>.
@@ -33,10 +76,24 @@ func mongoimport(t *testing.T, file, db, coll string) {
 		"--db="+db,
 		"--collection="+coll,
 		"--file="+file,
-		"--sort",
 		"--drop",
 		"--numInsertionWorkers=10",
 		"--stopOnError",
+		"mongodb://host.docker.internal:27017/",
+	)
+}
+
+func mongoexport(t *testing.T, file, db, coll string) {
+	t.Helper()
+
+	runDockerComposeCommand(
+		t,
+		"mongoexport",
+		"--verbose=2",
+		"--db="+db,
+		"--collection="+coll,
+		"--out="+file,
+		"--sort='{x:1}'",
 		"mongodb://host.docker.internal:27017/",
 	)
 }
