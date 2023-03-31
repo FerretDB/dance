926911f6537fbdad4148973a68ff542e18a27258
b1ron <goldingbyron@gmail.com>
2023-03-31 19:05:24 +0200

    rename runner

--- cmd/dance/main.go
+++ cmd/dance/main.go
@@ -32,7 +32,7 @@ import (
 	"github.com/FerretDB/dance/internal"
 	"github.com/FerretDB/dance/internal/gotest"
 	"github.com/FerretDB/dance/internal/jstest"
-	"github.com/FerretDB/dance/internal/service"
+	"github.com/FerretDB/dance/internal/srvtest"
 )
 
 func waitForPort(ctx context.Context, port uint16) error {
@@ -129,8 +129,8 @@ func main() {
 			runRes, err = gotest.Run(ctx, dir, config.Args, *vF)
 		case "jstest":
 			runRes, err = jstest.Run(ctx, dir, config.Args)
-		case "service":
-			runRes, err = service.Run(ctx, dir, config.Args)
+		case "srvtest":
+			runRes, err = srvtest.Run(ctx, dir, config.Args)
 		default:
 			log.Fatalf("unknown runner: %q", config.Runner)
 		}
