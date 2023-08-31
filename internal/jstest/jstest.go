3404a6013c26f8981672854f05c9c1c5642502cd
b1ron <goldingbyron@gmail.com>
2023-08-31 23:31:32 +0200

    comment

--- internal/jstest/jstest.go
+++ internal/jstest/jstest.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package jstest contains `mongo` runner.
+// Package jstest provides utilities to run JavaScript tests using the `mongo“ shell.
 package jstest
 
 import (
