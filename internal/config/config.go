ea0f5a251b929e8c35df037420d8e23df11bcc9a
b1ron <goldingbyron@gmail.com>
2023-10-10 13:16:52 +0200

    maintain the order

--- internal/config/config.go
+++ internal/config/config.go
@@ -193,10 +193,10 @@ func (tc *TestConfig) Compare(results *TestResults) (*CompareResult, error) {
 			switch testRes.Status {
 			case Fail:
 				compareResult.ExpectedFail[test] = testResOutput
-			case Pass:
-				compareResult.ExpectedPass[test] = testResOutput
 			case Skip:
 				compareResult.ExpectedSkip[test] = testResOutput
+			case Pass:
+				compareResult.ExpectedPass[test] = testResOutput
 			case Ignore:
 				fallthrough
 			case Unknown:
