62889bcb1103cd60dc469b6a488896754d049151
b1ron <goldingbyron@gmail.com>
2023-10-24 15:18:00 +0200

    add files

---
+++ tests/simple-todos-meteor.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -ex
+
+npm install
+
+meteor update
+
+export MONGO_URL="mongodb://localhost:27017/"
+
+# directConnection=true avoids server discovery
+export MONGO_OPLOG_URL="mongodb://localhost:27017/local?replicaSet=rs0&directConnection=true"
+
+meteor run
