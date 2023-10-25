3f523bf65119cf2b484c2832e086bdb6cac0d15f
b1ron <goldingbyron@gmail.com>
2023-10-25 16:52:06 +0200

    fix config and all oplog integration tests

--- tests/meteor.sh
+++ tests/meteor.sh
@@ -6,6 +6,10 @@ npm install -g npm@latest
 
 export PUPPETEER_DOWNLOAD_PATH=~/.npm/chromium
 
-# TODO add URL environment variables
-# TODO add more oplog tests
+export MONGO_URL="mongodb://localhost:27017/"
+
+export MONGO_OPLOG_URL="mongodb://localhost:27017/local?replicaSet=rs0&directConnection=true"
+
 TINYTEST_FILTER="mongo-livedata - oplog - cursorSupported" ./packages/test-in-console/run.sh
+TINYTEST_FILTER="mongo-livedata - oplog - entry skipping" ./packages/test-in-console/run.sh
+TINYTEST_FILTER="mongo-livedata - oplog - x - implicit collection creation" ./packages/test-in-console/run.sh
