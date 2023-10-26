c65933c436a8af8cf95c55fa8335dcf44fa956b2
b1ron <goldingbyron@gmail.com>
2023-10-26 13:07:27 +0200

    use --once and fix config

--- tests/meteor.sh
+++ tests/meteor.sh
@@ -10,6 +10,6 @@ export MONGO_URL="mongodb://localhost:27017/"
 
 export MONGO_OPLOG_URL="mongodb://localhost:27017/local?replicaSet=rs0&directConnection=true"
 
-TINYTEST_FILTER="mongo-livedata - oplog - cursorSupported" ./packages/test-in-console/run.sh
-TINYTEST_FILTER="mongo-livedata - oplog - entry skipping" ./packages/test-in-console/run.sh
-TINYTEST_FILTER="mongo-livedata - oplog - x - implicit collection creation" ./packages/test-in-console/run.sh
+TINYTEST_FILTER="mongo-livedata - oplog - cursorSupported" ./packages/test-in-console/run.sh --once
+TINYTEST_FILTER="mongo-livedata - oplog - entry skipping" ./packages/test-in-console/run.sh --once
+TINYTEST_FILTER="mongo-livedata - oplog - x - implicit collection creation" ./packages/test-in-console/run.sh --once
