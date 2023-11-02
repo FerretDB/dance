7294ec625292d8e52f99a21a23e6dad76ed6871e
b1ron <goldingbyron@gmail.com>
2023-11-02 05:51:53 +0100

    fix files

--- tests/meteor-oplog-entry-skipping.sh
+++ tests/meteor-oplog-entry-skipping.sh
@@ -10,7 +10,6 @@ export METEOR_LOCAL_DIR=~/.meteor
 
 export MONGO_URL="mongodb://localhost:27017/"
 
-# https://github.com/FerretDB/FerretDB/blob/main/cmd/envtool/envtool.go#L155
 export MONGO_OPLOG_URL="mongodb://localhost:27017/local?replicaSet=mongodb-rs&directConnection=true"
 
 TINYTEST_FILTER="mongo-livedata - oplog - entry skipping" ./packages/test-in-console/run.sh --once
