1e47044ff3f790ffc0f1ba37bc8ceabd8cf0749f
b1ron <goldingbyron@gmail.com>
2023-11-06 17:25:22 +0100

    clean file

--- tests/restheart.sh
+++ tests/restheart.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-# ensure we set JAVA_HOME to use Java 17
-# export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
-
 unset JAVA_HOME
 
 ./mvnw -X clean verify -DskipUTs -P-mongodb \
