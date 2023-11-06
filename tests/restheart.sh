67ca79250ad50ab6a9ed2cee44027b3d5ee70f9a
b1ron <goldingbyron@gmail.com>
2023-11-06 15:28:25 +0100

    debug maven

--- tests/restheart.sh
+++ tests/restheart.sh
@@ -5,8 +5,6 @@ set -ex
 # ensure we set JAVA_HOME to use Java 17
 export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 
-./mvnw clean package
-
-./mvnw clean verify -DskipUTs -P-mongodb \
+./mvnw -X clean verify -DskipUTs -P-mongodb \
 -Dtest-connection-string="mongodb://user:password@localhost/ferretdb?authMechanism=PLAIN" \
 -Dkarate.options="--tags ~@requires-replica-set"
