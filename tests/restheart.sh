d5d0dcb34a980f5773bb28ae1b63813b0275c2be
b1ron <goldingbyron@gmail.com>
2023-11-06 14:58:33 +0100

    build project

--- tests/restheart.sh
+++ tests/restheart.sh
@@ -5,6 +5,8 @@ set -ex
 # ensure we set JAVA_HOME to use Java 17
 export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 
+./mvnw clean package
+
 ./mvnw clean verify -DskipUTs -P-mongodb \
 -Dtest-connection-string="mongodb://user:password@localhost/ferretdb?authMechanism=PLAIN" \
 -Dkarate.options="--tags ~@requires-replica-set"
