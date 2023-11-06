5891b7b93ffefb07f3e9686b0c9021caffd8dad2
b1ron <goldingbyron@gmail.com>
2023-11-06 15:38:15 +0100

    rely on actions not JAVA_HOME

--- tests/restheart.sh
+++ tests/restheart.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # ensure we set JAVA_HOME to use Java 17
-export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+# export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 
 ./mvnw -X clean verify -DskipUTs -P-mongodb \
 -Dtest-connection-string="mongodb://user:password@localhost/ferretdb?authMechanism=PLAIN" \
