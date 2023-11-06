0903205de49751350812fcc5090fcf1414a86810
b1ron <goldingbyron@gmail.com>
2023-11-06 16:40:41 +0100

    try random things

--- tests/restheart.sh
+++ tests/restheart.sh
@@ -3,8 +3,10 @@
 set -ex
 
 # ensure we set JAVA_HOME to use Java 17
-export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+# export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 
-JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::") ./mvnw -X clean verify -DskipUTs -P-mongodb \
+unset $JAVA_HOME
+
+./mvnw -X clean verify -DskipUTs -P-mongodb \
 -Dtest-connection-string="mongodb://user:password@localhost/ferretdb?authMechanism=PLAIN" \
 -Dkarate.options="--tags ~@requires-replica-set"
