49042c15449020e30caf83c8f75392232dbe25f9
b1ron <goldingbyron@gmail.com>
2023-11-06 14:51:20 +0100

    set JAVA_HOME

--- tests/restheart.sh
+++ tests/restheart.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# ensure we set JAVA_HOME to use Java 17
+export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+
 ./mvnw clean verify -DskipUTs -P-mongodb \
 -Dtest-connection-string="mongodb://user:password@localhost/ferretdb?authMechanism=PLAIN" \
 -Dkarate.options="--tags ~@requires-replica-set"
