49bb1a753de601c68a922f010d898841aa106836
b1ron <goldingbyron@gmail.com>
2023-11-07 14:23:37 +0100

    auth

--- tests/restheart-auth.sh
+++ tests/restheart-auth.sh
@@ -5,5 +5,5 @@ set -ex
 unset JAVA_HOME
 
 ./mvnw -X clean verify -DskipUTs -P-mongodb \
--Dtest-connection-string="mongodb://localhost/ferretdb" \
+-Dtest-connection-string="mongodb://user:password@localhost/ferretdb?authMechanism=PLAIN" \
 -Dkarate.options="--tags ~@requires-replica-set"
