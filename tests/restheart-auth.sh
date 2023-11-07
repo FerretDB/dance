f058aea8c4844575c25d2908d11d18c52fda688d
b1ron <goldingbyron@gmail.com>
2023-11-07 15:07:59 +0100

    fix

--- tests/restheart-auth.sh
+++ tests/restheart-auth.sh
@@ -4,6 +4,8 @@ set -ex
 
 unset JAVA_HOME
 
+./mvnw clean package
+
 ./mvnw -X clean verify -DskipUTs -P-mongodb \
 -Dtest-connection-string="mongodb://user:password@localhost/ferretdb?authMechanism=PLAIN" \
 -Dkarate.options="--tags ~@requires-replica-set"
--- tests/restheart.sh
+++ tests/restheart.sh
@@ -4,6 +4,8 @@ set -ex
 
 unset JAVA_HOME
 
+./mvnw clean package
+
 ./mvnw -X clean verify -DskipUTs -P-mongodb \
 -Dtest-connection-string="mongodb://localhost/ferretdb" \
 -Dkarate.options="--tags ~@requires-replica-set"
