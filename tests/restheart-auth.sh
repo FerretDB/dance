e19161dd523ff7c54a46b21d1cefba3db4f750f3
b1ron <goldingbyron@gmail.com>
2023-11-09 06:32:07 +0100

    remove debug flag and mongo passes now

--- tests/restheart-auth.sh
+++ tests/restheart-auth.sh
@@ -6,6 +6,6 @@ unset JAVA_HOME
 
 ./mvnw clean package
 
-./mvnw -X clean verify -DskipUTs -P-mongodb \
+./mvnw clean verify -DskipUTs -P-mongodb \
 -Dtest-connection-string="mongodb://user:password@localhost/ferretdb?authMechanism=PLAIN" \
 -Dkarate.options="--tags ~@requires-replica-set"
--- tests/restheart.sh
+++ tests/restheart.sh
@@ -6,6 +6,6 @@ unset JAVA_HOME
 
 ./mvnw clean package
 
-./mvnw -X clean verify -DskipUTs -P-mongodb \
+./mvnw clean verify -DskipUTs -P-mongodb \
 -Dtest-connection-string="mongodb://localhost/ferretdb" \
 -Dkarate.options="--tags ~@requires-replica-set"
