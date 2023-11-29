e0b6d0d7391a0c1ae70bf51cf454003a3cb36f45
b1ron <80292536+b1ron@users.noreply.github.com>
2023-11-29 08:05:21 +0100

    Add oplog meteor integration tests (#634)
    Closes #633.
    Closes #248.

---
+++ tests/meteor-doc-fetcher.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+export PUPPETEER_DOWNLOAD_PATH=~/.npm/chromium
+
+export METEOR_LOCAL_DIR=~/.meteor
+
+export MONGO_URL="mongodb://localhost:27017/"
+
+export MONGO_OPLOG_URL="mongodb://localhost:27017/local?replicaSet=mongodb-rs&directConnection=true"
+
+TINYTEST_FILTER="mongo-livedata - doc fetcher" ./packages/test-in-console/run.sh --once
--- tests/restheart.sh
+++ tests/restheart.sh
@@ -6,6 +6,4 @@ unset JAVA_HOME
 
 ./mvnw clean package
 
-./mvnw clean verify -DskipUTs -P-mongodb \
--Dtest-connection-string="mongodb://localhost/ferretdb" \
--Dkarate.options="--tags ~@requires-replica-set"
+./mvnw clean verify -DskipUTs -P-mongodb -Dtest-connection-string="mongodb://localhost/ferretdb"
