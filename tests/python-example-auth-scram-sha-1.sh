8ee7ab231a676461802cb7a0e17667a861c322ba
b1ron <goldingbyron@gmail.com>
2024-01-17 16:38:43 +0100

    try

--- tests/python-example-auth-scram-sha-1.sh
+++ tests/python-example-auth-scram-sha-1.sh
@@ -4,4 +4,4 @@ set -ex
 
 pip3 install pymongo
 
-python3 pymongo_test.py mongodb://user:password@127.0.0.1:27017/?authMechanism=SCRAM-SHA-1
+python3 pymongo_test.py 'mongodb://user:password@localhost:27017/?directConnection=true&replicaSet=rs0&authMechanism=SCRAM-SHA-1'
