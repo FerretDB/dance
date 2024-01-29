dafd42362acc7b6fee71ffe8f4a3e119613e9e20
b1ron <80292536+b1ron@users.noreply.github.com>
2024-01-29 16:00:54 +0100

    Add SCRAM tests (#738)
    Closes #681.

--- tests/python-example-auth.sh
+++ tests/python-example-auth.sh
@@ -4,4 +4,4 @@ set -ex
 
 pip3 install pymongo
 
-python3 pymongo_test.py mongodb://user:password@localhost:27017/?authMechanism=PLAIN
+python3 pymongo_test.py 'mongodb://user:password@localhost:27017/?authMechanism=PLAIN'
