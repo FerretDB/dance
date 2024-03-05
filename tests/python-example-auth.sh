ae3c47ad7db2a87135c8ab7b9114a79942d82976
b1ron <goldingbyron@gmail.com>
2024-03-05 11:18:39 +0100

    fix pip command

--- tests/python-example-auth.sh
+++ tests/python-example-auth.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-pip3 install pymongo
+pip3 install -r requirements.txt
 
 python3 pymongo_test.py 'mongodb://user:password@localhost:27017/?authMechanism=PLAIN'
--- tests/python-example.sh
+++ tests/python-example.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-pip3 install pymongo
+pip3 install -r requirements.txt
 
 python3 pymongo_test.py mongodb://localhost:27017/
