07d6a47215d88b33b32066a1aec5364739e27ad2
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2024-04-15 17:57:16 +0400

    Remove

--- tests/java-example-auth-scram-sha-256.sh
+++ tests/java-example-auth-scram-sha-256.sh
@@ -2,8 +2,7 @@
 
 set -ex
 
-# enables Maven exceptions
-export MAVEN_OPTS='-ea'
+export MAVEN_OPTS='-enableassertions'
 
 mvn compile exec:java -Dexec.mainClass=com.start.Connection \
     -Dexec.args="mongodb://user:password@localhost:27017/?replicaSet=rs0&authMechanism=SCRAM-SHA-256"
