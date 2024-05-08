5e7a03441507c3694b3baf246dfec3001b84146d
b1ron <goldingbyron@gmail.com>
2024-05-08 16:28:55 +0200

    fix uri

--- tests/java-example-scram.sh
+++ tests/java-example-scram.sh
@@ -5,7 +5,7 @@ set -ex
 export MAVEN_OPTS='-enableassertions'
 
 mvn compile exec:java -Dexec.mainClass=com.start.Connection \
-    -Dexec.args="mongodb://user:password@localhost:27017/?authMechanism=SCRAM-SHA-1"
+    -Dexec.args="mongodb://user:password@localhost:27017/?replicaSet=rs0&authMechanism=SCRAM-SHA-1"
 
 mvn compile exec:java -Dexec.mainClass=com.start.Connection \
-    -Dexec.args="mongodb://user:password@localhost:27017/?authMechanism=SCRAM-SHA-256"
+    -Dexec.args="mongodb://user:password@localhost:27017/?replicaSet=rs0&authMechanism=SCRAM-SHA-256"
