3aadabcc78a06065e3e00d6e4045c4689ae0d2b7
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2024-08-19 20:25:29 +0400

    Allow PostgreSQL image customization

--- projects/_old/dotnet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-dotnet run 'mongodb://localhost:27017/?replicaSet=rs0'
--- projects/_old/java.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-export MAVEN_OPTS='-enableassertions'
-
-mvn compile exec:java -Dexec.mainClass=com.start.Connection -Dexec.args="mongodb://localhost:27017/?replicaSet=rs0"
